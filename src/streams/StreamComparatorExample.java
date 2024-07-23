941d9dc0c90e6f61a4e40edb4869cc1fc6c1ed19
Nilesh_Modi <nilesh.modi.nitb@gmail.com>
2024-07-23 16:11:34 +0530

    Implement Stream Comparator Example or sorted(Comparator.comparing())

---
+++ src/streams/StreamComparatorExample.java
@@ -0,0 +1,39 @@
+package streams;
+
+import data.Student;
+import data.StudentDataBase;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamComparatorExample {
+
+    //Sort all students based on their names
+    public static List<Student> getSortedListByNames(){
+
+        //way 1
+//        return StudentDataBase.getAllStudents().stream()
+//                .sorted((o1, o2) -> o1.getName().compareTo(o2.getName()))
+//                .collect(Collectors.toList());
+
+        //way 2
+        return StudentDataBase.getAllStudents().stream()
+                .sorted(Comparator.comparing(Student::getName))
+                .collect(Collectors.toList());
+    }
+
+    //Sort all students based on their gpa
+    public static List<Student> getSortedListByGpa(){
+        return StudentDataBase.getAllStudents().stream()
+                .sorted(Comparator.comparing(Student::getGpa))
+                .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println("Sorted Students by Name : "+ getSortedListByNames());
+        System.out.println("Sorted Students by GPA : "+ getSortedListByGpa());
+
+    }
+}
